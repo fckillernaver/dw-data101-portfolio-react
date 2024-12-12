db392b3e6edfacba24fa9b5652794be444f7270f
fckillernaver <fckiller@naver.com>
2024-12-13 08:31:18 +0900

    'update'

--- src/screens/Home/Contact.jsx
+++ src/screens/Home/Contact.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./Contact.css";
 
 const Contact = () => {
-  return <div className="contact">Contact</div>;
+  return (
+    <div className="contact">
+      <ul>
+        <li>
+          <a href="mailto:dexteryoon@icloud.com">dexteryoon@icloud.com</a>
+          <a href="https://github.com/fckillernaver" target="_blank">
+            visit Dexter's Github
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Contact;
